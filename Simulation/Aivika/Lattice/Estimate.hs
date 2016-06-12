4405ddc73ec5ace75005dbd3a3730dc2202e3dd3
David Sorokin <david.sorokin@gmail.com>
2016-06-12 08:28:09 +0300

    Added function memoEstimate.

--- Simulation/Aivika/Lattice/Estimate.hs
+++ Simulation/Aivika/Lattice/Estimate.hs
@@ -26,4 +26,33 @@ module Simulation.Aivika.Lattice.Estimate
         -- * Debugging
         traceEstimate) where
 
+import Simulation.Aivika.Trans.Internal.Types
+import Simulation.Aivika.Trans.Simulation
+import Simulation.Aivika.Trans.Parameter
 import Simulation.Aivika.Lattice.Internal.Estimate
+import Simulation.Aivika.Lattice.Internal.LIO
+import qualified Simulation.Aivika.Lattice.Internal.Ref as R
+
+-- | Estimate the event computation in the lattice nodes by the specified time.
+memoEstimate :: (Estimate LIO a -> Estimate LIO a)
+                -- ^ estimate in the intermediate time point of the lattice
+                -> Estimate LIO a
+                -- ^ estimate in the final time point of the lattice or beyond it
+                -> Simulation LIO (Estimate LIO a)
+memoEstimate f m =
+  do r  <- R.newRef Nothing
+     t2 <- liftParameter stoptime
+     let loop =
+           Estimate $ \p ->
+           do b <- R.readRef0 r
+              case b of
+                Just a  -> return a
+                Nothing ->
+                  if pointTime p >= t2
+                  then do a <- invokeEstimate p m
+                          R.writeRef0 r (Just a)
+                          return a
+                  else do a <- invokeEstimate p (f loop)
+                          R.writeRef0 r (Just a)
+                          return a
+     return loop
